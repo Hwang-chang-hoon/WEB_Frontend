65b7d15251326df278b590757a6d4a4a31d78d5b
Hwang-chang-hoon <49145440+Hwang-chang-hoon@users.noreply.github.com>
2022-08-24 05:23:26 +0900

    [#11] components Combine

--- components/RecruitQuestion.js
+++ components/RecruitQuestion.js
@@ -1,15 +1,21 @@
 //질문 작성 페이지
+import { TextField } from '@mui/material';
 import { Box, Button } from '@mui/material';
-import React, { Component } from 'react';
+import React, { Component, useCallback } from 'react';
 import { useState, useRef } from 'react';
 import QuestionForm from '../components/QuestionForm.js';
-import QuestionInfoList from '../components/QuestionInfoList';
+import QuestionInfo from './QuestionInfo.js';
 import styles from '../public/css/questionAdminStyle.module.css';
 import { defaultFadeInLeftVariants, defaultFadeInUpVariants } from './motion';
 import SendIcon from '@mui/icons-material/Send';
 import { motion } from 'framer-motion';
 
 const RecruitQuestion = (props) => {
+  RecruitQuestion.defaultProps = {
+    data: [],
+    onRemove: () => console.warn('onRemove not defined'),
+  };
+
   const [input, setInput] = useState({
     question: '',
   });
@@ -25,7 +31,12 @@ const RecruitQuestion = (props) => {
     },
   ]);
 
+  const handleQuestion = useCallback((e) => {
+    setQuestion(e.target.value);
+  });
+
   const nextId = useRef(2);
+
   const handleCreate = (data) => {
     const user = {
       id: nextId.current,
@@ -36,20 +47,39 @@ const RecruitQuestion = (props) => {
     setInput({
       question: '',
     });
+
     nextId.current += 1;
     console.log(data);
     console.log(question);
   };
+
+  const handleSubmit = (e) => {
+    // 페이지 리로딩 방지
+    e.preventDefault();
+    // 상태값을 onCreate 를 통하여 부모에게 전달
+    props.onCreate(question);
+    // 상태 초기화
+    setQuestion('');
+  };
+
   const handleRemove = (id) => {
     setQuestion(question.filter((info) => info.id !== id));
   };
+
   return (
     <div className={`${styles.Wrap}`}>
       <motion.h2 initial="initial" whileInView="animate" variants={defaultFadeInLeftVariants} className={`${styles.recruitName}`}>
         {props.count}기 지원서
       </motion.h2>
+
       <QuestionForm onCreate={handleCreate} />
-      <QuestionInfoList data={question} onRemove={handleRemove} />
+
+      <div className={`${styles.Wrap}`}>
+        {question.map((info) => (
+          <QuestionInfo key={info.id} info={info} onRemove={handleRemove} />
+        ))}
+      </div>
+
       <motion.div initial="initial" whileInView="animate" variants={defaultFadeInUpVariants} className={`${styles.RecruitAnswerSubmitWrap}`}>
         <Button variant="text" size="large" className={`${styles.RecruitAnswerSubmit}`} endIcon={<SendIcon />}>
           제출
